bbfd1d7d99eef9b104b3939cd3892a47e325e87e
Dennis Haegler <d.haegler@hotmail.de>
2015-03-22 19:45:02 +0100

    solve kata

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -5,11 +5,25 @@ describe WordCounter do
   let(:test_string_1) do
     "e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"
   end
+  let(:string_with_slash) { " //wont won't won't " }
 
   context '4 words given' do
     it 'returns the word count' do
       result = ["e", "ddd", "aa"]
       expect(word_counter.top_3_words(test_string_1)).to eq result
+    end
+  end
+
+  context 'when slash containing' do
+    it 'return the word without slash' do
+      result = ["won't", "wont"]
+      expect(word_counter.top_3_words(string_with_slash)).to eq result
+    end
+  end
+
+  context 'when only single quote given' do
+    it 'return empty string' do
+      expect(word_counter.top_3_words("'")).to eq []
     end
   end
 end
--- word_counter.rb
+++ word_counter.rb
@@ -1,4 +1,19 @@
 class WordCounter
   def top_3_words(text)
+    return [] unless text =~ /[a-zA-Z]/
+    text = text.gsub('.', '').gsub(',', '').gsub(':', '').gsub('/', '')
+    text.downcase!
+    words = text.split ' '
+    word_count = {}
+    words.each do |word|
+      if word_count.keys.include?(word)
+        word_count[word] += 1
+      else
+        word_count[word] = 1
+      end
+    end
+    result = word_count.sort_by {|_key, value| value}.reverse
+    result.map! {|key_value| key_value.first}
+    result.first 3
   end
 end
