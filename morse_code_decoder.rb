91264ab0bae795f8ecacfc66c6afdb2f5b1c2b2e
Dennis Haegler <d.haegler@hotmail.de>
2015-03-23 22:28:13 +0100

    add more solutions

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,31 +1,14 @@
 class MorseCodeDecoder
   MORSE_CODE = {
-    '.-': 'a',
-    '-...': 'b',
-    '-.-.': 'c',
-    '-..': 'd',
-    '.': 'e',
-    '..-.': 'f',
-    '--.': 'g',
-    '....': 'h',
-    '..': 'i',
-    '.---': 'j',
-    '-.-': 'k',
-    '.-..': 'l',
-    '--': 'm',
-    '-.': 'n',
-    '---': 'o',
-    '.--.': 'p',
-    '--.-': 'q',
-    '.-.': 'r',
-    '...': 's',
-    '-': 't',
-    '..-': 'u',
-    '...-': 'v',
-    '.--': 'w',
-    '-..-': 'x',
-    '-.--': 'y',
-    '--..':'z'
+    ".-" => "a", "-..." => "b", "-.-." => "c",
+    "-.." => "d", "." => "e", "..-." => "f",
+    "--." => "g", "...." => "h", ".." => "i",
+    ".---" => "j", "-.-" => "k", ".-.." => "l",
+    "--" => "m", "-." => "n", "---" => "o",
+    ".--." => "p", "--.-" => "q", ".-." => "r",
+    "..." => "s", "-" => "t", "..-" => "u",
+    "...-" => "v", ".--" => "w", "-..-" => "x",
+    "-.--" => "y", "--.." => "z"
   }
 
   def decode_morse(morse)
@@ -33,10 +16,17 @@ class MorseCodeDecoder
     morse.split('   ').each do |word|
       single_word = ''
       word.split.each do |letter|
-        single_word << MORSE_CODE[letter.to_sym]
+        return '' unless MORSE_CODE[letter]
+        single_word << MORSE_CODE[letter]
       end
       result << single_word
     end
     result.join(" ").upcase
+  end
+
+  def decode_bits(bits)
+    bits = bits.to_s.gsub('000000000000', '   ')
+    bits = bits.gsub('111111', '-').gsub('000000', ' ')
+    bits.gsub("11", '.').gsub('00', '')
   end
 end
--- spec/morse_code_decoder_spec.rb
+++ spec/morse_code_decoder_spec.rb
@@ -4,9 +4,50 @@ describe MorseCodeDecoder do
   let(:decoder) { MorseCodeDecoder.new }
 
   describe '.decode_morse' do
-    it 'returns human readable text' do
-      morse = '.... . -.--   .--- ..- -.. .'
-      expect(decoder.decode_morse(morse)).to eq 'HEY JUDE'
+    context 'sentence given' do
+      it 'returns human readable text' do
+        morse = '.... . -.--   .--- ..- -.. .'
+        expect(decoder.decode_morse(morse)).to eq 'HEY JUDE'
+      end
+    end
+
+    context 'single word given' do
+      it 'returns human readable text' do
+        morse = '... --- ...'
+        expect(decoder.decode_morse(morse)).to eq 'SOS'
+      end
+    end
+
+    context 'wrong morse code given' do
+      it 'return empty string' do
+        morse = '... ---- ...'
+        expect(decoder.decode_morse(morse)).to eq ''
+      end
+    end
+  end
+
+  describe '.decode_bits' do
+    context 'decoded H given' do
+      it 'return morse code' do
+        h_as_bits = 11001100110011
+        expect(decoder.decode_bits(h_as_bits)).to eq '....'
+      end
+    end
+
+    context 'decoded SOS given' do
+      it 'return morse code' do
+        h_as_bits = 110011001100000011111100111111001111110000001100110011
+        expect(decoder.decode_bits(h_as_bits)).to eq '... --- ...'
+      end
+    end
+
+    context 'decoded HEY JUDE given' do
+      it 'returns morse code' do
+        bits = '110011001100110000001100000011111100110011111100111111'\
+        '000000000000001100111111001111110011111100000011001100111111000'\
+        '0001111110011001100000011'
+        expect(decoder.decode_bits(bits)).to eq '.... . -.--   .--- ..- -.. .'
+      end
     end
   end
 end
