a75b7561a9e7358d748db1ee091f296044b0ecc0
Dennis Haegler <d.haegler@hotmail.de>
2015-05-11 19:34:03 +0200

    finish kata

--- data_accessor_container.rb
+++ data_accessor_container.rb
@@ -1,2 +1,22 @@
-class DataAccessorContainer
+class SuperClass
+  attr_accessor :data
+
+  def initialize
+    @data = {}
+  end
+
+  def self.data_accessor(*args)
+    args.each do |arg|
+      define_method("#{arg}=") do |method_argument|
+        data[arg] = method_argument
+      end
+      define_method(arg) do
+        data[arg]
+      end
+    end
+  end
+end
+
+class DataAccessorContainer < SuperClass
+  data_accessor :foo
 end
--- spec/data_accessor_container_spec.rb
+++ spec/data_accessor_container_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 require './data_accessor_container'
 
 describe DataAccessorContainer do
+  let(:data_container) { DataAccessorContainer.new }
+
+  it 'defines a method' do
+    expect(data_container.respond_to? :foo).to be true
+    expect(data_container.respond_to? :foo=).to be true
+    expect(data_container.respond_to? :bullshit).to be false
+  end
+
+  it 'sets value to defined method' do
+    data_container.foo = 10
+    expect(data_container.foo).to be 10
+  end
 end
