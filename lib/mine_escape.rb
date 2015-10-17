23488eb0d07b09941f9a454caccf589c0d23d1d6
Dennis Haegler <dennis.haegler@gmail.com>
2015-10-17 13:34:04 +0200

    Test commit

--- lib/mine_escape.rb
+++ lib/mine_escape.rb
@@ -1,3 +1,13 @@
 def solve(map, current_pos, escape_point)
-  []
+  return [] if map.empty?
+  navigation = []
+  while map[current_pos['x'] + 1][current_pos['y']]
+    navigation << 'right'
+    current_pos['x'] += 1;
+  end
+  while map[current_pos['x']][current_pos['y'] + 1]
+    navigation << 'down'
+    current_pos['y'] += 1;
+  end
+  navigation
 end
--- spec/mine_escape_spec.rb
+++ spec/mine_escape_spec.rb
@@ -1,7 +1,12 @@
 require 'mine_escape'
 
 describe '#solve' do
-  it { expect(solve([], { x: 0, y: 0 }, { x: 0, y: 0 })).to eq [] }
+  it 'should excape 2x2 matrix' do
+    mine_map = [[true, false], [true, true]]
+    current = {'x'=> 0,'y'=> 0}
+    goal = {'x'=> 1,'y'=> 1 }
+    expect(solve(mine_map, current, goal)).to eq(%w{right down})
+  end
 
   context '3x3 matrix given' do
     let(:empty_map) { Array.new(3, Array.new(3, false)) }
@@ -14,15 +19,16 @@ describe '#solve' do
     end
 
     it 'returns [] with false only' do
-      current_pos = { x: 0 , y: 0}
-      exit_pos = { x: 0 , y: 0}
+      current_pos = { 'x'=> 0 , 'y'=> 0}
+      exit_pos = { 'x'=> 0 , 'y'=> 0}
       expect(solve(map, current_pos, exit_pos)).to eq []
     end
 
+
     it 'return array of rights to goal' do
-      current_pos = { x: 0 , y: 0}
-      exit_pos = { x: 2 , y: 0}
-      expect(solve(map, current_pos, exit_pos)).to eq(%w{right right right})
+      current_pos = { 'x'=> 0 , 'y'=> 0}
+      exit_pos = { 'x'=> 2 , 'y'=> 0}
+      expect(solve(map, current_pos, exit_pos)).to eq(%w{down down right righ up up})
     end
   end
 end
